e56d7d2a7e93c47cc6cafa5cc52949fd500eb0a2
Valentin Omnes <valentin.omnes@gmail.com>
2018-10-15 21:23:35 +0200

    feat: Add carousel tp

---
+++ Carousel/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <Carousel/>
+  </div>
+</template>
+
+<script>
+import Carousel from './components/Carousel';
+
+export default {
+  name: 'App',
+  components: {
+    Carousel,
+  },
+};
+</script>
+
+<style>
+</style>
