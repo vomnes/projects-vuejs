8757907b1eddcccd695713837004a4e41f04db07
Valentin Omnes <valentin.omnes@gmail.com>
2018-10-15 22:43:05 +0200

    feat: Start carousel

--- Carousel/src/App.vue
+++ Carousel/src/App.vue
@@ -1,16 +1,27 @@
 <template>
   <div id="app">
-    <Carousel/>
+    <carousel>
+      <carousel-slide>
+        Salut tous le monde
+        <img src="http://lorempicsum.com/nemo/627/200/3">
+      </carousel-slide>
+      <carousel-slide>
+        Au revoir tous le monde
+        <img src="http://lorempicsum.com/up/627/200/3">
+      </carousel-slide>
+    </carousel>
   </div>
 </template>
 
 <script>
-import Carousel from './components/Carousel';
+import Carousel from './components/carousel/Carousel';
+import CarouselSlide from './components/carousel/CarouselSlide';
 
 export default {
   name: 'App',
   components: {
     Carousel,
+    CarouselSlide
   },
 };
 </script>
---
+++ Carousel/src/components/carousel/CarouselSlide.vue
@@ -0,0 +1,26 @@
+<template>
+  <div v-show="visible">
+    Index: {{ index }}
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    data () {
+      return {
+        index: 0
+      }
+    },
+    computed: {
+      visible () {
+        return this.index === this.$parent.index
+      }
+    }
+  }
+
+</script>
+
+<style lang="css">
+</style>
